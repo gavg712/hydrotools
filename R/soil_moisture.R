222d80dec5424afaf2700a4d654a8776229947bd
Gabo Gaona <gavg712@gmail.com>
2019-12-13 11:44:24 -0500

    added soil moisture data descriptor

---
+++ R/soil_moisture.R
@@ -0,0 +1,14 @@
+#' Soil moisture data for examples.
+#'
+#' A dataset containing the data needed for a hydrograph with hyetograph.
+#'
+#' @format A tibble with 95630 rows and 3 variables:
+#' \describe{
+#'   \item{timestamp}{Timestamp with breaks each 5 min}
+#'   \item{runoff}{Fraction of water content per volume of soil}
+#'   \item{rain}{Total rainfall in 5 min}
+#'
+#' }
+#' @author Gabriel Gaona
+#' @source \url{http://www.tmf_respect.org/}
+"soil_moisture"
